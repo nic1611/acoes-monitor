d5e18635ce62878640d79345d1035ee54d15a87f
nic1611 <nisoffi@gmail.com>
2021-02-14 18:07:47 -0300

    uepa

--- webappacoes.py
+++ webappacoes.py
@@ -11,7 +11,7 @@ st.write(
 st.sidebar.header("Escolha sua ação")
 
 def get_data():
-    path = 'C:/Users/fabri/Google Drive/codifike/23 - WebAppAcoes/all_bovespa.csv'
+    path = 'COTAHIST/all_bovespa.csv'
     return pd.read_csv(path)
 
 df = get_data()
@@ -24,3 +24,23 @@ max_date = max(df_data)
 stock = df['sigla_acao'].drop_duplicates()
 
 stock_choices = st.sidebar.selectbox('Escolha sua ação', stock)
+
+start_date = st.sidebar.text_input('Digite uma data de início', min_date)
+end_date = st.sidebar.text_input('Digite uma data de final', max_date)
+
+start = pd.to_datetime(start_date)
+end = pd.to_datetime(end_date)
+
+if start > end:
+    st.error('Data final deve ser maior que a data inicial')
+
+df = df[(df['sigla_acao'] == stock_choices) & (pd.to_datetime(df['data_pregao']) >= start) & (pd.to_datetime(df['data_pregao']) <= end)]
+
+df = df.set_index(pd.DatetimeIndex(df['data_pregao'].values))
+
+st.header('Ação: ' + stock_choices.upper())
+st.write('Preço de fechamento')
+st.line_chart(df['preco_fechamento'])
+
+st.write('Volume negociado')
+st.line_chart(df['volume_negocios'])
